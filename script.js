35d8994368deee369d0053988e70d94e6aa1af05
Alban <alban.berth@gmail.com>
2020-11-05 00:23:20 +0100

    test javascript 7

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-/*
-function getCP(){
-    var temp;
-    temp = document.getElementById("CP").value;
-    return temp
-}
-*/
-
 function nodejs(){
     alert("Hello worl");
-    
-    app.get('/', function(req, resp) {
-        connection.query("SELECT CDR.CP, CDR.NomCom, InfoCom.Population, InfoCom.ScoreGlobalCom, InfoCom.AccesInterfaceNum, InfoCom.AccesInformation, InfoCom.CompAdministrative, InfoCom.CompNumerique, InfoCom.GlobalAcces, InfoCom.GlobalCompetences, CDR.NomDep, CDR.NomRegion, InfoCom.ScoreGlobalRegion FROM InfoCom, CDR, InfoCom_CDR WHERE InfoCom.CodeIris = InfoCom_CDR.CodeIris AND InfoCom_CDR.INSEE = CDR.INSEE AND CDR.CP = 49750;", function(error, rows, fields) {
-            if (!!error)  {
-                console.log('Error in the query');
-            } else {
-                console.log('Successfull query');
-                resp.send(rows);
-            }
-        });
-    });
-    
-/*
-    var sql = "SELECT CDR.CP, CDR.NomCom, InfoCom.Population, InfoCom.ScoreGlobalCom, InfoCom.AccesInterfaceNum, InfoCom.AccesInformation, InfoCom.CompAdministrative, InfoCom.CompNumerique, InfoCom.GlobalAcces, InfoCom.GlobalCompetences, CDR.NomDep, CDR.NomRegion, InfoCom.ScoreGlobalRegion FROM InfoCom, CDR, InfoCom_CDR WHERE InfoCom.CodeIris = InfoCom_CDR.CodeIris AND InfoCom_CDR.INSEE = CDR.INSEE AND CDR.CP = ?";
-    connection.query(sql, [CP], function(error, rows, fields) {
-        if (!!error)  {
-            console.log('Error in the query');
-        } else {
-            console.log('Successfull query');
-            resp.send(rows);
-        }
-    });
-*/
 }
