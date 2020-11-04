aef9daf86c39a60285746904509ff67c59a5d8e0
Alban <alban.berth@gmail.com>
2020-11-04 20:22:55 +0100

    Afficher index avec node.js

--- myfirst.js
+++ myfirst.js
@@ -2,5 +2,18 @@ var http = require('http');
 
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello World!');
+    //res.end('Hello World!');
+    response.sendFile( __dirname  + '/LOGIN.js');
 }).listen(8080);
+
+/*
+var http = require('http'); //On créé un serveur web
+var express = require('express'); //On va récupèrer la page qui sera demandée
+ 
+var server=express();
+server.listen(8000);
+ 
+server.get('/LOGIN', function(request, response) {
+    response.sendFile( __dirname  + '/LOGIN.js');
+});
+*/
