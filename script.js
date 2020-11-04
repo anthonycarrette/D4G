a367d09d8ac939152b8a546566a8c03691405c70
Alban <alban.berth@gmail.com>
2020-11-04 21:27:28 +0100

    Ajout sendFile 2.0

--- script.js
+++ script.js
@@ -19,13 +19,13 @@ connection.connect(function(error) {
     }
 });
 
-app.get('/', function(req, resp) {
+app.get('/index.html', function(req, resp) {
     connection.query("SELECT CDR.CP, CDR.NomCom, InfoCom.Population, InfoCom.ScoreGlobalCom, InfoCom.AccesInterfaceNum, InfoCom.AccesInformation, InfoCom.CompAdministrative, InfoCom.CompNumerique, InfoCom.GlobalAcces, InfoCom.GlobalCompetences, CDR.NomDep, CDR.NomRegion, InfoCom.ScoreGlobalRegion FROM InfoCom, CDR, InfoCom_CDR WHERE InfoCom.CodeIris = InfoCom_CDR.CodeIris AND InfoCom_CDR.INSEE = CDR.INSEE AND CDR.CP = 49750;", function(error, rows, fields) {
         if (!!error)  {
             console.log('Error in the query');
         } else {
             console.log('Successfull query');
-            res.sendFile('./index.html');
+            resp.sendFile(rows);
         }
     });
 });
