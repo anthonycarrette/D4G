3e129bdaad297ee23f8fbfe6bf7273f3971c8ef5
Alban <alban.berth@gmail.com>
2020-11-04 20:25:59 +0100

    Afficher index avec node.js 2.0

--- myfirst.js
+++ myfirst.js
@@ -3,7 +3,7 @@ var http = require('http');
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     //res.end('Hello World!');
-    response.sendFile( __dirname  + '/LOGIN.js');
+    response.sendFile('index.html');
 }).listen(8080);
 
 /*
