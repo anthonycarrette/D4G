5a433349e504e998b1e23499b5d1ba44374e7d23
Alban <alban.berth@gmail.com>
2020-11-05 08:42:41 +0100

    un truc 7

--- init.js
+++ init.js
@@ -2,6 +2,11 @@ var express = require('express');
 var mysql = require('mysql');
 var app = express();
 
+
+app.get('/', function(req, resp){
+    resp.sendFile( __dirname + '/index.html');
+});
+
 var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -17,24 +22,7 @@ connection.connect(function(error) {
     }
 });
 
-/*
-app.get('/', function(req, resp){
-    resp.sendFile( __dirname + '/index.html');
-});
-*/
-/*
-app.get('/', function(req, resp) {
-    connection.query("SELECT CDR.CP, CDR.NomCom, InfoCom.Population, InfoCom.ScoreGlobalCom, InfoCom.AccesInterfaceNum, InfoCom.AccesInformation, InfoCom.CompAdministrative, InfoCom.CompNumerique, InfoCom.GlobalAcces, InfoCom.GlobalCompetences, CDR.NomDep, CDR.NomRegion, InfoCom.ScoreGlobalRegion FROM InfoCom, CDR, InfoCom_CDR WHERE InfoCom.CodeIris = InfoCom_CDR.CodeIris AND InfoCom_CDR.INSEE = CDR.INSEE AND CDR.CP = 49750;", function(error, rows, fields) {
-        if (!!error)  {
-            console.log('Error in the query');
-        } else {
-            console.log('Successfull query');
-            resp.send(rows);
-        }
-    });
-});
-*/
-var CP = 49000;
+var CP = 49750;
 var sql = "SELECT CDR.CP, CDR.NomCom, InfoCom.Population, InfoCom.ScoreGlobalCom, InfoCom.AccesInterfaceNum, InfoCom.AccesInformation, InfoCom.CompAdministrative, InfoCom.CompNumerique, InfoCom.GlobalAcces, InfoCom.GlobalCompetences, CDR.NomDep, CDR.NomRegion, InfoCom.ScoreGlobalRegion FROM InfoCom, CDR, InfoCom_CDR WHERE InfoCom.CodeIris = InfoCom_CDR.CodeIris AND InfoCom_CDR.INSEE = CDR.INSEE AND CDR.CP = ?";
 app.get('/', function(req, resp) {
     connection.query(sql, [CP], function(error, rows, fields) {
